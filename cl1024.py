a9d2d1d9c9da47a27b3af2168b5cf6e7cac705b8
crazyjudas <502485441@qq.com>
2015-12-27 01:46:33 +0800

    the first test

--- cl1024.py
+++ cl1024.py
@@ -1,12 +1,23 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+import re
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 class Crxdy(CrawlSpider):
-	name = 'tech.163'
-	allowed.domains = ['tech.163.com']
-	start_urls = ['http://tech.163.com/special/0009rt/tech_it.html']
+	name = 'cl1024'
+	allowed_domains = ['cl.weod.pw']
+	start_urls = ['http://cl.weod.pw/thread0806.php?fid=22&search=&page=1']
 	rules = (
-		Rule
+		Rule(LinkExtractor(allow='thread0806\.php\?fid=22&search=&page=(\d+)'), 'parse_list', follow=True,),
 	)
+	
+	def parse_list(self, response):
+		self.logger.info('Hi, this is an list page %s', response.url)
+		#title  response.xpath('//tr/td/h3/a/text()')
+		#href  response.xpath('//tr/td/h3/a/@href') 
+		#data storage nesting and export to csv 
+		item = scrapy.Item()
+		
